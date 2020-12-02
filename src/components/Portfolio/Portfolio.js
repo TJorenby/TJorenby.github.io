b50c7cdb2da28528e64961a2adcbe18ca5564300
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-12-02 10:31:37 -0600

    Added theWeather app to portfolio. Implemented ToolTips

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,10 +7,13 @@ import imageGalleryImg from "./PortfolioImages/image-gallery.png";
 import imageGalleryGif from "./PortfolioImages/image-gallery-gif.gif";
 import movieGalleryGif from "./PortfolioImages/movie-gallery-gif.gif";
 import movieGalleryImg from "./PortfolioImages/movie-gallery.png";
+import theWeatherImg from "./PortfolioImages/theWeather.png";
+import theWeatherGif from "./PortfolioImages/theWeather-demo.gif";
 import myTodayImg from "./PortfolioImages/myToday-app.png";
 
 // Style Imports
 import Modal from "@material-ui/core/Modal";
+import Tooltip from "@material-ui/core/Tooltip";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSpring, animated } from "react-spring";
 
@@ -47,6 +50,7 @@ function Portfolio() {
   const [modalOpenB, setModalOpenB] = useState(false);
   const [modalOpenC, setModalOpenC] = useState(false);
   const [modalOpenD, setModalOpenD] = useState(false);
+  const [modalOpenE, setModalOpenE] = useState(false);
 
   return (
     <animated.div style={trans}>
@@ -60,13 +64,16 @@ function Portfolio() {
         {/* Features Main */}
         <section className="portfolio-main">
           <div className="container grid">
+            {/* myToday */}
             <div className="card">
               <p className="lead text-bold md">myToday</p>
-              <img
-                src={myTodayImg}
-                alt=""
-                onClick={() => setModalOpenA(true)}
-              />
+              <Tooltip title="Click to view demo" interactive>
+                <img
+                  src={myTodayImg}
+                  alt=""
+                  onClick={() => setModalOpenA(true)}
+                />
+              </Tooltip>
               {/* <p>React | Node.js | Express | Redux | PostgreSQL</p> */}
               <p className="sm">
                 A styled-for-mobile app designed to allow users to take photos
@@ -76,18 +83,59 @@ function Portfolio() {
               <a
                 href="https://github.com/TJorenby/prime-solo-project-mytoday"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab fa-github fa-2x my-2 i"></i>
               </a>
             </div>
 
+            {/* theWeather */}
+            <div className="card">
+              {/* <p className="lead text-bold md">theWeather App</p> */}
+
+              <a
+                href="https://theweather-app-jorenby.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Tooltip title="Click to use the app!" interactive>
+                  <p className="lead text-bold md">theWeather App</p>
+                </Tooltip>
+              </a>
+
+              <Tooltip title="Click to view demo" interactive>
+                <img
+                  src={theWeatherImg}
+                  alt=""
+                  onClick={() => setModalOpenB(true)}
+                />
+              </Tooltip>
+              {/* <p>React | Node.js | Express | Redux | PostgreSQL</p> */}
+
+              <p className="sm">
+                Powered by weatherAPI, this app allows users to access weather
+                conditions anywhere in the world.
+              </p>
+
+              <a
+                href="https://github.com/TJorenby/weather-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github fa-2x my-3 i"></i>
+              </a>
+            </div>
+
+            {/* Feedback Survey */}
             <div className="card">
               <p className="lead text-bold md">Feedback Survey</p>
-              <img
-                src={feedbackLoopImg}
-                alt=""
-                onClick={() => setModalOpenB(true)}
-              />
+              <Tooltip title="Click to view demo" interactive>
+                <img
+                  src={feedbackLoopImg}
+                  alt=""
+                  onClick={() => setModalOpenC(true)}
+                />
+              </Tooltip>
               {/* <p>React | Node.js | Express | Redux | PostgreSQL</p> */}
               <p className="sm">
                 A feedback form that allows users to answer questions in the
@@ -96,6 +144,31 @@ function Portfolio() {
               <a
                 href="https://github.com/TJorenby/redux-feedback-loop"
                 target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github fa-2x my-3 i"></i>
+              </a>
+            </div>
+
+            {/* Movie Carousel */}
+            <div className="card">
+              <p className="lead text-bold md">Movie Carousel</p>
+              <Tooltip title="Click to view demo" interactive>
+                <img
+                  src={movieGalleryImg}
+                  alt=""
+                  onClick={() => setModalOpenD(true)}
+                />
+              </Tooltip>
+              {/* <p>React | Node.js | Express | Redux | PostgreSQL</p> */}
+              <p className="sm">
+                A movie library that allows users to browse titles, learn more
+                about each title, and upload new titles to a database.
+              </p>
+              <a
+                href="https://github.com/TJorenby/movie-sagas"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab fa-github fa-2x my-3 i"></i>
               </a>
@@ -103,11 +176,13 @@ function Portfolio() {
 
             <div className="card">
               <p className="lead text-bold md">Image Gallery</p>
-              <img
-                src={imageGalleryImg}
-                alt=""
-                onClick={() => setModalOpenC(true)}
-              />
+              <Tooltip title="Click to view demo" interactive>
+                <img
+                  src={imageGalleryImg}
+                  alt=""
+                  onClick={() => setModalOpenC(true)}
+                />
+              </Tooltip>
               {/* <p>React | Node.js | Express | PostgreSQL</p> */}
               <p className="sm">
                 An app that allows users to add images, "love" them, or "delete"
@@ -116,25 +191,9 @@ function Portfolio() {
               <a
                 href="https://github.com/TJorenby/react-gallery"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab fa-github fa-2x my-2 i"></i>
-              </a>
-            </div>
-
-            <div className="card">
-              <p className="lead text-bold md">Movie Carousel</p>
-              <img
-                src={movieGalleryImg}
-                alt=""
-                onClick={() => setModalOpenD(true)}
-              />
-              {/* <p>React | Node.js | Express | Redux | PostgreSQL</p> */}
-              <p className="sm">
-                A movie library that allows users to browse titles, learn more
-                about each title, and upload new titles to a database.
-              </p>
-              <a href="https://github.com/TJorenby/movie-sagas" target="_blank">
-                <i className="fab fa-github fa-2x my-3 i"></i>
               </a>
             </div>
           </div>
@@ -149,25 +208,32 @@ function Portfolio() {
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
+              rel="noopener noreferrer"
             ></iframe>
           </div>
         </Modal>
 
         <Modal open={modalOpenB} onClose={() => setModalOpenB(false)}>
           <div style={modalStyle} className={classes.paper}>
-            <img src={feedbackLoopGif} alt="" />
+            <img src={theWeatherGif} alt="" />
           </div>
         </Modal>
 
         <Modal open={modalOpenC} onClose={() => setModalOpenC(false)}>
           <div style={modalStyle} className={classes.paper}>
-            <img src={imageGalleryGif} alt="" />
+            <img src={feedbackLoopGif} alt="" />
           </div>
         </Modal>
 
         <Modal open={modalOpenD} onClose={() => setModalOpenD(false)}>
           <div style={modalStyle} className={classes.paper}>
             <img src={movieGalleryGif} alt="" />
+          </div>
+        </Modal>
+
+        <Modal open={modalOpenE} onClose={() => setModalOpenE(false)}>
+          <div style={modalStyle} className={classes.paper}>
+            <img src={imageGalleryGif} alt="" />
           </div>
         </Modal>
       </div>
