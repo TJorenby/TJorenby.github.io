6b1dd443677acd7b473d7bfa82daee6bad99b633
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-12-07 17:00:46 -0600

    removed gh pages dependencies. Updated BrowserRouter to HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import AboutMe from "../AboutMe/AboutMe";
 import Contact from "../Contact/Contact";
